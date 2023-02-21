4297d4e74c68984d636e35ca7760ce50395e0673
Lucas Martins <lucasrodriguesmartinsoliveira@gmail.com>
2023-02-21 16:17:23 -0300

    Commit inicial

---
+++ src/main/java/com/lucas/github/financial_planning/FinancialPlanningApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.github.financial_planning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialPlanningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancialPlanningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucas/github/financial_planning/FinancialPlanningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucas.github.financial_planning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialPlanningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
