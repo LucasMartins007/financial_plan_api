1448e1ad4aafdfc7bfbda3db97c21fe382c0abdb
Lucas Martins <lucasrodriguesmartinsoliveira@gmail.com>
2023-02-22 16:33:08 -0300

    resolvendo code smell de duplicação de código

--- src/main/java/com/lucas/github/financial_planning/model/dtos/generic/AbstractDTO.java
+++ src/main/java/com/lucas/github/financial_planning/model/dtos/generic/AbstractDTO.java
@@ -1,51 +1,12 @@
 package com.lucas.github.financial_planning.model.dtos.generic;
 
-import com.lucas.github.financial_planning.model.entity.generic.AbstractEntity;
-import com.lucas.github.financial_planning.model.pattern.IIdentifier;
+import com.lucas.github.financial_planning.model.pattern.AbstractModelClass;
+import lombok.Getter;
+import lombok.Setter;
 
-public abstract class AbstractDTO<T extends Number> implements IIdentifier<T> {
+@Getter
+@Setter
+public abstract class AbstractDTO<T extends Number> extends AbstractModelClass<T> {
 
-    @Override
-    public String toString() {
-        return this.getClass().getSimpleName() + "(id=" + getId() + ")";
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-
-        if (this == obj) {
-            return true;
-        }
-
-        if (this.getClass() != obj.getClass()) {
-            return false;
-        }
-
-        @SuppressWarnings("unchecked")
-        final AbstractEntity<T> other = (AbstractEntity<T>) obj;
-        if (this.getId() != null && other.getId() == null) {
-            return false;
-        }
-
-        if (other.getId() != null && this.getId() == null) {
-            return false;
-        }
-
-        if (this.getId() == null && other.getId() == null) {
-            return false;
-        }
-
-        return this.getId().equals(other.getId());
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 5;
-        hash = 71 * hash + (this.getId() != null ? this.getId().hashCode() : 0);
-        return hash;
-    }
 
 }
--- src/main/java/com/lucas/github/financial_planning/model/entity/generic/AbstractEntity.java
+++ src/main/java/com/lucas/github/financial_planning/model/entity/generic/AbstractEntity.java
@@ -1,5 +1,6 @@
 package com.lucas.github.financial_planning.model.entity.generic;
 
+import com.lucas.github.financial_planning.model.pattern.AbstractModelClass;
 import com.lucas.github.financial_planning.model.pattern.IIdentifier;
 import jakarta.persistence.*;
 import lombok.Getter;
@@ -9,7 +10,7 @@ import java.util.Date;
 
 @Getter
 @Setter
-public abstract class AbstractEntity<T extends Number> implements IIdentifier<T> {
+public abstract class AbstractEntity<T extends Number> extends AbstractModelClass<T> {
 
     @Column(name = "include_date")
     @Temporal(TemporalType.DATE)
@@ -34,46 +35,4 @@ public abstract class AbstractEntity<T extends Number> implements IIdentifier<T>
         this.updateDate = new Date();
     }
 
-    @Override
-    public String toString() {
-        return this.getClass().getSimpleName() + "(id=" + getId() + ")";
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-
-        if (this == obj) {
-            return true;
-        }
-
-        if (this.getClass() != obj.getClass()) {
-            return false;
-        }
-
-        @SuppressWarnings("unchecked")
-        final AbstractEntity<T> other = (AbstractEntity<T>) obj;
-        if (this.getId() != null && other.getId() == null) {
-            return false;
-        }
-
-        if (other.getId() != null && this.getId() == null) {
-            return false;
-        }
-
-        if (this.getId() == null && other.getId() == null) {
-            return false;
-        }
-
-        return this.getId().equals(other.getId());
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 5;
-        hash = 71 * hash + (this.getId() != null ? this.getId().hashCode() : 0);
-        return hash;
-    }
 }
