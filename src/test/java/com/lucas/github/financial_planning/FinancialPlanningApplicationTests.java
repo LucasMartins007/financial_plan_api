da81993d7eb69aebd9ff435e490d88b9f9a8d772
Lucas Martins <lucasrodriguesmartinsoliveira@gmail.com>
2023-02-22 11:26:35 -0300

    Retirado carregamento do contexto do projeto nos testes unitários

--- src/test/java/com/lucas/github/financial_planning/FinancialPlanningApplicationTests.java
+++ src/test/java/com/lucas/github/financial_planning/FinancialPlanningApplicationTests.java
@@ -3,7 +3,6 @@ package com.lucas.github.financial_planning;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class FinancialPlanningApplicationTests {
 
     @Test
