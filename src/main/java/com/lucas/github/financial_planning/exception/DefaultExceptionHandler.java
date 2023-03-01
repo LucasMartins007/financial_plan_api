940ef04d3022067bbc501622d7c1fe4e2120deb6
Lucas Martins <lucasrodriguesmartinsoliveira@gmail.com>
2023-02-28 23:21:04 -0300

    Correção na padronização de erros da aplicação

--- src/main/java/com/lucas/github/financial_planning/exception/DefaultExceptionHandler.java
+++ src/main/java/com/lucas/github/financial_planning/exception/DefaultExceptionHandler.java
@@ -3,21 +3,15 @@ package com.lucas.github.financial_planning.exception;
 import com.lucas.github.financial_planning.exception.enums.EnumMessagesException;
 import com.lucas.github.financial_planning.exception.model.DefaultExceptionResponse;
 import com.lucas.github.financial_planning.exception.runtime.DomainRuntimeException;
-import jakarta.annotation.Priority;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.http.*;
-import org.springframework.lang.NonNull;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
-import java.util.List;
 
 @ControllerAdvice
 public class DefaultExceptionHandler extends ResponseEntityExceptionHandler {
@@ -33,26 +27,6 @@ public class DefaultExceptionHandler extends ResponseEntityExceptionHandler {
 
         return deafultException(defaultExceptionResponse, HttpStatus.BAD_REQUEST);
     }
-
-    @Override
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(
-            MethodArgumentNotValidException ex,
-            @NonNull HttpHeaders headers,
-            @NonNull HttpStatusCode status,
-            @NonNull WebRequest request) {
-        final List<String> missingFields = ex.getBindingResult().getFieldErrors().stream()
-                .map(FieldError::getField)
-                .toList();
-
-        final DefaultExceptionResponse defaultExceptionResponse = new DefaultExceptionResponse(EnumMessagesException.MISSING_FIELDS_EXCEPTION.getMessage());
-        defaultExceptionResponse.setMissingFields(missingFields);
-        defaultExceptionResponse.setDateError(new Date());
-        defaultExceptionResponse.setStatus(EnumMessagesException.MISSING_FIELDS_EXCEPTION.getStatusCode());
-
-        return exception(defaultExceptionResponse, HttpStatus.resolve(EnumMessagesException.MISSING_FIELDS_EXCEPTION.getStatusCode()));
-    }
-
 
     @ExceptionHandler(NullPointerException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
@@ -71,11 +45,5 @@ public class DefaultExceptionHandler extends ResponseEntityExceptionHandler {
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(exceptionResponse);
     }
-
-    private ResponseEntity<Object> exception(DefaultExceptionResponse exceptionResponse, HttpStatus httpStatus) {
-        return ResponseEntity
-                .status(httpStatus)
-                .contentType(MediaType.APPLICATION_JSON)
-                .body(exceptionResponse);
-    }
+    
 }
