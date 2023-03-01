91356ff61230ddfc91a571acd86c1f00bf42a747
Lucas Martins <lucasrodriguesmartinsoliveira@gmail.com>
2023-02-28 23:19:14 -0300

    Retirado imports não utilizados, ajuste anotações do lombok

--- src/main/java/com/lucas/github/financial_planning/model/entity/generic/AbstractEntity.java
+++ src/main/java/com/lucas/github/financial_planning/model/entity/generic/AbstractEntity.java
@@ -1,15 +1,14 @@
 package com.lucas.github.financial_planning.model.entity.generic;
 
 import com.lucas.github.financial_planning.model.pattern.AbstractModelClass;
-import com.lucas.github.financial_planning.model.pattern.IIdentifier;
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.Date;
 
 @Getter
 @Setter
+@EqualsAndHashCode(callSuper = true)
 public abstract class AbstractEntity<T extends Number> extends AbstractModelClass<T> {
 
     @Column(name = "include_date")
